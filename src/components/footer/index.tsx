84587d82ea326498ffc4be4cf5405abc2fe6387a
Kashif Nazar <muhammad.kashif.nazar@gmail.com>
2022-08-26 23:09:53 +0100

    Adding the footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import Navigation from '../navigation'
+
+import './styles.css'
 
 type Props = {}
 
 const Footer = (props: Props) => {
   return (
-    <footer>Footer</footer>
+    <footer>
+        <div className='flexbox vertical'>
+          <img src='/images/black-logo.png' alt='Your logo' />
+          <p>©2021  Company Name <br/>
+            All rights reserved</p>
+        </div>
+        <Navigation></Navigation>
+    </footer>
   )
 }
 
